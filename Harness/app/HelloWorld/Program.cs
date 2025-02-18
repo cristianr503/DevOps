06300c6661977f7339e3b861b23b10d8003eacc0
cristianr503@gmail.com <8623337+cristianr503@users.noreply.github.com>
2025-02-18 12:59:55 -0500

    initial commit

---
+++ Harness/app/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, World! Running on {RuntimeInformation.OSDescription} - {RuntimeInformation.OSArchitecture}");
+        }
+    }
+}
